0bfbc4064bd3afcfe60d720d3f905ba15321ac33
mobin <mobinmohammadi4321@gmail.com>
2025-06-13 20:48:45 +0330

    fkfacs

--- src/Pages/Home/CategurtByProducts/CategurtByProducts.jsx
+++ src/Pages/Home/CategurtByProducts/CategurtByProducts.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-import CategurtProducts from '../../../assets/Components/CategurtProducts/CategurtProducts'
+import React from "react";
+import CategurtProducts from "../../../assets/Components/CategurtProducts/CategurtProducts";
+import TopBarMobile from "../../../assets/Components/TopBarMobile/TopBarMobile.";
 
 export default function CategurtByProducts() {
   return (
-    <CategurtProducts/>
-  )
+    <>
+      <TopBarMobile />
+      <div className="container-custom">
+        <CategurtProducts />
+      </div>
+    </>
+  );
 }
--- src/assets/Components/CategurtProducts/CategurtProducts.jsx
+++ src/assets/Components/CategurtProducts/CategurtProducts.jsx
@@ -6,28 +6,51 @@ import TopBarMobile from "../TopBarMobile/TopBarMobile.";
 import { useParams } from "react-router-dom";
 
 export default function CategurtProducts() {
-
-    const categuryName = useParams()
-    console.log(categuryName.categuryProduct)
-    let title = ""
-    switch(categuryName.categuryProduct){
-        case  "protein" :
-         title = "پروتعین وی"
-         break
-        default : {
-            title = ""
-        }
+  const categuryName = useParams();
+  console.log(categuryName.categuryProduct);
+  let title = "";
+  switch (categuryName.categuryProduct) {
+    case "protein":
+      title = "پروتعین وی";
+      break;
+    case "amino":
+      title = "آمینو";
+      break;
+    case "gainer":
+      title = "گینر";
+      break;
+    case "carbomas":
+      title = "کربو مس";
+      break;
+    case "ceratin":
+      title = "کراتین";
+      break;
+    case "testboaster":
+      title = "تست بوستر";
+      break;
+    case "glotamin":
+      title = "گلوتامین";
+      break;
+    case "moltivitamin":
+      title = "مولتی ویتامین";
+      break;
+    case "bcaa":
+      title = "بی سی ای ای";
+      break;
+    default: {
+      title = "";
     }
-    
-    
+  }
+
   return (
     <div className="flex">
-      <TopBarMobile />
       <div className="mt-22 w-full">
-        <RoadMapSite categury="دسته بندی ها" name={title}   />
-        <div className="flex gap-5">
-          <FilterCategurtProducts />
-          <ContentCategurtProducts />
+        <RoadMapSite categury="دسته بندی ها" name={title} />
+        <div className="container-custom">
+          <div className="flex gap-5 mt-10 ">
+            <FilterCategurtProducts />
+            {/* <ContentCategurtProducts /> */}
+          </div>
         </div>
       </div>
     </div>
